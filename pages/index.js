281e4c156f7416cba3b0faca2314cf02feb7dfd8
Aluno <RotomDaPesteBR>
2022-06-06 11:24:53 -0300

    1.0

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,80 @@
 import styles from '../styles/index.module.css'
-import { Container, Col, Row } from 'react-bootstrap';
+import { useReducer, useState } from 'react';
+import React, { Component } from 'react'
+import { Container, Col, Row, Form, Button, ButtonGroup } from 'react-bootstrap';
 
-export default function Home() {
+export default function Home(){
+    const [valor1, setValor1] = useState(0);
+    const [valor2, setValor2] = useState(0);
+    const [resultado, setResultado] = useState(0);
+    const [operacao, setOperacao] = useState("+");
 
+    function calcular(){
+        let x1 = parseFloat(valor1);
+        let x2 = parseFloat(valor2);
+        if (operacao == "+"){
+            setResultado(x1+x2);
+        }else if (operacao == "-"){
+            setResultado(x1-x2);
+        }else if (operacao == "*"){
+            setResultado(x1*x2);
+        }else if (operacao == "/"){
+            setResultado(x1/x2);
+        }
+    }
 
-    return  <div>
+    return  <div style={stylesheet.content}>
+                <Form style={stylesheet.form}>
+                    <Col>
+                    Número 1:
+                    <Form.Control style={stylesheet.input} type="number" placeholder="" value={valor1} onChange={e => setValor1(e.target.value)}/>
+                    Número 2:
+                    <Form.Control style={stylesheet.input} type="number" placeholder="" value={valor2} onChange={e => setValor2(e.target.value)}/>
+                    <br></br>
+                    <ButtonGroup style={stylesheet.botao}>
+                        <Button onClick={()=>setOperacao("+")} style={stylesheet.operacao} variant="secondary">+</Button>
+                        <Button onClick={()=>setOperacao("-")}style={stylesheet.operacao} variant="secondary">-</Button>
+                        <Button onClick={()=>setOperacao("*")} style={stylesheet.operacao} variant="secondary">×</Button>
+                        <Button onClick={()=>setOperacao("/")} style={stylesheet.operacao} variant="secondary">÷</Button>
+                    </ButtonGroup>
+                    <br></br>
+                    <br></br>
+                    <Button onClick={()=>calcular()} style={stylesheet.botao} variant="primary">
+                    Calcular
+                    </Button>
+                    </Col>
+                </Form>
+                <br></br>
+                <p style={stylesheet.resultado}>{resultado}</p>
+                
                 
             </div> 
+}
+
+const stylesheet = {
+    content:{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        width: "100%",
+    },
+
+    form:{
+        padding: "100px",
+    },
+
+    input: {
+        textAlign: "center",
+    },
+
+    operacao:{
+        fontSize: "20px",
+    },
+
+    botao: {
+        width: "100%",
+    },
+    resultado: {
+        textAlign: "center",
+    },
 }
